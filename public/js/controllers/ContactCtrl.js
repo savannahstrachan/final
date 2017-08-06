5ec2046f946c122a243bf97db0797ef62af771a3
Savannah Strachan <ENTER YOUR ACTUAL EMAIL ADDRESS>
2017-08-06 12:04:05 -0500

    form revision

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -51,53 +51,16 @@ angular.module('ContactCtrl', []).controller('ContactController', function($scop
                     }
                 }
             },
-            address: {
-                validators: {
-                     stringLength: {
-                        min: 8,
-                    },
-                    notEmpty: {
-                        message: 'Please supply your street address'
-                    }
-                }
-            },
-            city: {
-                validators: {
-                     stringLength: {
-                        min: 4,
-                    },
-                    notEmpty: {
-                        message: 'Please supply your city'
-                    }
-                }
-            },
-            state: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please select your state'
-                    }
-                }
-            },
-            zip: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please supply your zip code'
-                    },
-                    zipCode: {
-                        country: 'US',
-                        message: 'Please supply a vaild zip code'
-                    }
-                }
-            },
+            
             comment: {
                 validators: {
                       stringLength: {
                         min: 10,
                         max: 200,
-                        message:'Please enter at least 10 characters and no more than 200'
+                        message:'Please enter no more than 200 characters'
                     },
                     notEmpty: {
-                        message: 'Please supply a description of your project'
+                        message: ''
                     }
                     }
                 }
