a20af0f0319b384ba395c6c735e7148a428f7ace
Savannah Strachan <ENTER YOUR ACTUAL EMAIL ADDRESS>
2017-08-07 09:05:08 -0500

    added views

--- public/js/controllers/AlumniCtrl.js
+++ public/js/controllers/AlumniCtrl.js
@@ -1,22 +1,9 @@
 angular.module('AlumniCtrl', []).controller('AlumniController', function($scope) {
 
-	$scope.tagline = 'Nothing beats a pocket protector!';
+    $scope.tagline = 'Nothing beats a pocket protector!';
 
+});
 
-
-
-		$scope.addInfo = function(response){
-			
-			console.log($scope.inputData);
-    	
-    	$http.post("https://final-project-loyno.herokuapp.com//add-alumni",
-    		$scope.inputData).then(function(err, res){
-
-    			$scope.inputData = {};
-    			// res.send($scope.inputData);
-    		})
-        	}
-        });
 
 
 
